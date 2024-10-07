12f7a9c98db77f31a87f8ba8de261bacf8b041bc
Ben Stolovitz <ben@stolovitz.com>
2024-10-07 19:31:05 -0400

    bin dir

---
+++ src/kits.rs
@@ -0,0 +1,43 @@
+use std::path::PathBuf;
+use std::env;
+
+fn get_kit_dir() -> PathBuf {
+    let program_files_x86 = env::var("ProgramFiles(x86)").unwrap();
+    let base_path = PathBuf::from(program_files_x86).join("Windows Kits");
+    base_path
+}
+
+pub fn get_kit_bin_dirs() -> Vec<PathBuf> {
+    let kit_dir = get_kit_dir();
+    let bin_dir = kit_dir.join("10").join("bin");
+
+    // List all the directories in the bin directory
+    //
+    // Should look like:
+    //
+    // * C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0
+    // * C:\Program Files (x86)\Windows Kits\10\bin\10.0.22000.0
+    // * ...
+    // * arm (with some weird XAML DLLs)
+    // * arm64 (ditto)
+    // * x64 (ditto)
+    // * x86 (ditto)
+    //
+    // We filter out the non-"version number" directories
+    let mut bin_dirs = Vec::new();
+    for entry in bin_dir.read_dir().unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        const BAD_PATHS: [&str; 4] = ["arm", "arm64", "x64", "x86"];
+
+        if path.is_dir() && !BAD_PATHS.contains(&path.file_name().unwrap().to_str().unwrap()) {
+            bin_dirs.push(path);
+        }
+    }
+
+    // Sort the directories by version number
+    bin_dirs.sort();
+
+    bin_dirs
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
+mod kits;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args
+{
+    // TODO: well-known bins?
+    binary: String,
+
+    // TODO: well-known archs?
+    architecture: String,
+
+    kit_version: Option<String>,
+
+    // TODO: list all kits?
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let bin_dirs = kits::get_kit_bin_dirs();
+    let most_recent = bin_dirs.last().unwrap();
+
+    println!("Hello, {0} {1}!", args.binary, most_recent.display());
 }
