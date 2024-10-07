eb860b03df8102afa264d7c457d2a2c611b4f1e2
Ben Stolovitz <ben@stolovitz.com>
2024-10-07 19:33:47 -0400

    I can get accevent.exe

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ struct Args
     binary: String,
 
     // TODO: well-known archs?
-    architecture: String,
+    architecture: Option<String>,
 
     kit_version: Option<String>,
 
@@ -19,9 +19,19 @@ struct Args
 
 fn main() {
     let args = Args::parse();
+    let architecture = args.architecture.unwrap_or("x64".to_string());
 
     let bin_dirs = kits::get_kit_bin_dirs();
     let most_recent = bin_dirs.last().unwrap();
 
-    println!("Hello, {0} {1}!", args.binary, most_recent.display());
+    let tool_path = most_recent.join(architecture).join(args.binary);
+
+    // If the tool doesn't exist, print an error message and exit
+    if !tool_path.exists() {
+        eprintln!("Error: tool not found: {}", tool_path.display());
+        std::process::exit(1);
+    }
+
+    // Print the path to the tool
+    println!("{}", tool_path.display());
 }
