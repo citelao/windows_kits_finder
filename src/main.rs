175b53b35a47d7fd327d84396dfa5b0a42b54a67
Ben Stolovitz <ben@stolovitz.com>
2024-10-07 20:16:11 -0400

    start custom error...

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use clap::Parser;
 use colored::*;
 use kits::get_kit_dir;
+use thiserror::Error;
 
 mod kits;
 
@@ -28,7 +29,28 @@ struct Args
     kit_dir: Option<String>,
 }
 
-fn do_it(args: Args) -> Result<(), Box<dyn Error>> {
+#[derive(Error, Debug)]
+pub enum OurError {
+    #[error("kit version not found: {desired} (maybe you want {potential}?)")]
+    BinDirNotFound{ desired: String, potential: String },
+
+    #[error("tool not found: {0}")]
+    ToolNotFound(String),
+
+    // #[error("data store disconnected")]
+    // Disconnect(#[from] io::Error),
+    // #[error("the data for key `{0}` is not available")]
+    // Redaction(String),
+    // #[error("invalid header (expected {expected:?}, found {found:?})")]
+    // InvalidHeader {
+    //     expected: String,
+    //     found: String,
+    // },
+    // #[error("unknown data store error")]
+    // Unknown,
+}
+
+fn do_it(args: Args) -> Result<(), OurError> {
     let architecture = args.architecture.unwrap_or("x64".to_string());
 
     let kit_dir_to_use = args.kit_dir.map_or_else(|| get_kit_dir(), |dir| std::path::PathBuf::from(dir));
@@ -40,16 +62,13 @@ fn do_it(args: Args) -> Result<(), Box<dyn Error>> {
         } else {
             // Get leaf folder name
             let latest_version = bin_dirs.last().map(|dir| dir.file_name().unwrap().to_str().unwrap());
-
-            let error = format!("{}: kit version not found: {}; maybe you want '{}'", "Error".bold(), kit_version, latest_version.unwrap());
-            eprintln!("{}", error.red());
-            std::process::exit(1);
+            return Err(OurError::BinDirNotFound{ desired: kit_version, potential: latest_version.unwrap().to_string() });
         }
     } else {
         bin_dirs.last().unwrap()
     };
 
-    let tool_path = bin_dir_to_use.join(architecture).join(args.binary);
+    let tool_path = bin_dir_to_use.join(architecture).join(args.binary.clone());
 
     // If the tool doesn't exist, print an error message and exit
     if !tool_path.exists() {
@@ -58,9 +77,7 @@ fn do_it(args: Args) -> Result<(), Box<dyn Error>> {
             let warning = format!("Warning: tool not found: {}", tool_path.display());
             eprintln!("{}", warning.yellow());
         } else {
-            let error = format!("{}: tool not found: {}", "Error".bold(), tool_path.display());
-            eprintln!("{}", error.red());
-            std::process::exit(1);
+            return Err(OurError::ToolNotFound(args.binary));
         }
     }
 
@@ -71,8 +88,14 @@ fn do_it(args: Args) -> Result<(), Box<dyn Error>> {
 
 fn main() {
     let args = Args::parse();
-    let result = do_it(args);
-    result.unwrap();
+    match do_it(args) {
+        Ok(_) => std::process::exit(0),
+        Err(e) => {
+            let error = format!("{}: {}", "Error".bold(), e);
+            eprintln!("{}", error.red());
+            std::process::exit(1);
+        }
+    }
 }
 
 #[cfg(test)]
